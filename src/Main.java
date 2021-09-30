4058ec873c48e03bab9b7edf6b4b906d208733f8
Anouk Driessen <info@anoukdriessen.nl>
2021-09-30 19:36:22 +0200

    setup nieuwe poging

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main klasse van het Casino
+ * opdracht van Full Stack Bootcamp - Backend NOVI
+ * September 2021
+ * @author anouk driessen
+ */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welkom bij het Casino de Gouden driehoek");
+    }
+}
